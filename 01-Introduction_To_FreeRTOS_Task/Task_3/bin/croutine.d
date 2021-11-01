7e26e3c5cdc49d44706d2bcb4fd95552484dc999
mohamedfab <mo7amed.fawzy33@gmail.com>
2021-11-01 23:29:33 +0200

    Task3_Initial_Commit

---
+++ 01-Introduction_To_FreeRTOS_Task/Task_3/bin/croutine.d
@@ -0,0 +1,14 @@
+.\bin\croutine.o: Source\croutine.c
+.\bin\croutine.o: .\Source\include\FreeRTOS.h
+.\bin\croutine.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\bin\croutine.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\bin\croutine.o: ..\Task_1\FreeRTOSConfig.h
+.\bin\croutine.o: C:\Keil_v5\ARM\INC\Philips\lpc21xx.h
+.\bin\croutine.o: .\Source\include\projdefs.h
+.\bin\croutine.o: .\Source\include\portable.h
+.\bin\croutine.o: .\Source\include\deprecated_definitions.h
+.\bin\croutine.o: .\Source\include\..\..\Source\portable\RVDS\ARM7_LPC21xx\portmacro.h
+.\bin\croutine.o: .\Source\include\mpu_wrappers.h
+.\bin\croutine.o: .\Source\include\task.h
+.\bin\croutine.o: .\Source\include\list.h
+.\bin\croutine.o: .\Source\include\croutine.h
